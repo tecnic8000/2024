4aade44ea0b825acf6b7af99fe2b56eb36694307
hARRi <tranhoangtd@gmail.com>
2024-03-12 00:51:34 +0700

    1

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+p1 = Point(4,5)
 print('helloWorld\n')
 a = (1, 4, 5)
 b = {1, 2, 4}
-print(type(a), type(b))
+print(type(a), type(b), p1.y)
