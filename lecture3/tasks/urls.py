e422a4748a39fb40eee69a7b9adcdde169a8b8cb
hARRi <tranhoangtd@gmail.com>
2024-03-13 23:06:02 +0700

    .

--- lecture3/tasks/urls.py
+++ lecture3/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
-urlspatterns = [
-    path('', views.index, name='index')
+app_name = 'tasks'
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add', views.add, name='add')
 ]
