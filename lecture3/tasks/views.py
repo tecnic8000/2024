00a567782aa72031385c9da1b6f3fc2b09a6a56e
hARRi <tranhoangtd@gmail.com>
2024-03-13 18:38:15 +0700

    .

--- lecture3/tasks/views.py
+++ lecture3/tasks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+tasks = ['food', 'bar', 'baz']
 # Create your views here.
+def index(request):
+    return render(request, 'tasks/index.html', {
+        'tasks': tasks
+    })
