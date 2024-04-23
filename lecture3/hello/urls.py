165f751b1c3a4c62d1f2600d3296ec5892277c66
hARRi <tranhoangtd@gmail.com>
2024-04-23 11:47:26 +0700

    !

--- lecture3/hello/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('harri', views.harri, name='harri'),
-    path('<str:name1>', views.greet, name='greet')
-]
--- lecture3/newyear/views.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    now1 = datetime.datetime.now()
-    return render(request, 'newyear/index.html', {
-        'newyear': now1.month == 3 and now1.year == 2024
-    })
