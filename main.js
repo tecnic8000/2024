d7d53cce127b7add951cfe9e88bd178f7d4f1315
hARRi <tranhoangtd@gmail.com>
2024-05-09 21:55:38 +0700

    1

--- main.js
+++ main.js
@@ -1,29 +1,8 @@
-// Get the current time
-    function getCurrentTime() {
-        let now = new Date();
-        let hours = now.getHours();
-        let minutes = now.getMinutes();
-        let seconds = now.getSeconds();
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-        // Add leading zeros if needed
-        hours = (hours < 10 ? "0" : "") + hours;
-        minutes = (minutes < 10 ? "0" : "") + minutes;
-        seconds = (seconds < 10 ? "0" : "") + seconds;
 
-        // Format the time
-        let currentTimeString = hours + ":" + minutes + ":" + seconds;
 
-        return currentTimeString;
-    }
-
-    // Update the HTML with the current time
-    function updateTime() {
-        let currentTimeElement = document.getElementById("currentTime");
-        currentTimeElement.textContent = getCurrentTime();
-    }
-
-    // Update the time every second
-    //setInterval(updateTime, 1000);
-
-    // Initial call to display the time immediately
-    updateTime();
+const controls = new OrbitControls( camera, renderer.domElement );
+const loader = new GLTFLoader();
