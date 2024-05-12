e09f064844192357434a109f2b16d02ab5e8e3e3
hARRi <tranhoangtd@gmail.com>
2024-05-12 07:35:11 +0700

    1

--- chart/chart.js
+++ chart/chart.js
@@ -7,42 +7,62 @@ const data = {
     datasets: [{
       label: 'projName1', //PROJECT NAME
       data: [
-        {x: ['2022-05-02', '2022-05-15'], y: 'task1'},
-        {x: ['2022-05-03', '2022-05-25'], y: 'task2'},
-        {x: ['2022-05-02', '2022-05-05'], y: 'task3'},
-        {x: ['2022-05-12', '2022-05-30'], y: 'task4'},
-      ], backgroundColor: 'rgba(255, 26, 104, .6)'
+        {x: ['2022-05-02', '2022-05-15'], y: 'task1', name: 'James'},
+        {x: ['2022-05-03', '2022-05-25'], y: 'task2', name: 'James'},
+        {x: ['2022-05-02', '2022-05-05'], y: 'task3', name: 'James'},
+        {x: ['2022-05-12', '2022-05-30'], y: 'task4', name: 'James'},
+      ], 
+      backgroundColor: 'rgba(150, 26, 104, .5)',
+      barPercentage:0.6
     }]
   };
-  // todayLine plugin
+  //  plugin //not working yet
   const todayLine = {
     id: 'todayLine',
     afterDatasetsDraw(chart, args, pluginOptions) {
         const { ctx, data, chartArea: { top, bottom, left, right }, scales: {x,y}} = chart;
         ctx.save();
         ctx.beginPath();
-        ctx.lineWidth = 3 ;
+        ctx.lineWidth = 5 ;
+        ctx.strokeStyle = 'rgba(255, 26, 104, 1)';
+        ctx.setLineDash([6,6]);
         ctx.moveTo(x.getPixelForValue(new Date()), top);
         ctx.lineTo(x.getPixelForValue(new Date()), bottom);
-        ctx.stroke(); 
-  }
-}
+        ctx.stroke();
+        }
+    }
+    const assignedTask = { //this one works so far
+        id: 'assignTask',
+        afterDatasetsDraw(chart, args, pluginOptions) {
+            const { ctx, data, chartArea: { top, bottom, left, right }, scales: {x,y}} = chart;
+            ctx.font = 'bold';
+            ctx.fillStyle = 'black';
+            ctx.fillText('text', 10, y.getPixelForValue(0));
+        }
+    }
+
+
   // config 
   const config = {
     type: 'bar',
     data,
     options: {
+      layout: {},
       indexAxis : 'y',
       scales: {
         x: {
-          type: 'time',
-          time: {unit: 'day'},
-          min: '2022-04-21', //PROJECT START DATE
-          max: '2022-06-15' //PROJECT END DATE
+            position: 'top',
+            type: 'time',
+            time: {unit: 'day'},
+            min: '2022-04-27', //PROJECT START DATE
+            max: '2022-06-10' //PROJECT END DATE
         }
+      },
+      plugins:{
+        legend: {display:false}
       }
     },
-    plugins: [todayLine]
+    plugins: [todayLine, assignedTask]
   };
 
   // render init block
