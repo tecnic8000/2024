a51bd773c8af8e617ec0b3aa97d1d2981a5c534c
hARRi <tranhoangtd@gmail.com>
2024-05-12 23:28:26 +0700

    1

--- chart/chart.js
+++ chart/chart.js
@@ -1,20 +1,29 @@
 console.log("Chart.js " + Chart.version);
 //----REMEBER TO CHECK SCRIPT LINK IN HTML----
 //----TASK: BLOCKOUT, ANIMATIC, MODELLING, RIGGING, ANIMATION, VFX/LIGHT, RENDER, COMP, CLOSED
-//----STATUS: PLANNED, WORKING, REVIEW1, DONE, PENDING
 
+const colorCode = {
+    PENDING: 'rgba(255, 26, 104, 1)',
+    PLANNED: 'rgba(54, 162, 235, 1)',
+    WORKING: 'rgba(255, 206, 86, 1)',
+    DONE: 'rgba(75, 192, 192, 1)',
+    UNKNOWN: 'rgba(153, 102, 255, 1)',
+    REVIEW: 'rgba(255, 159, 64, 1)'
+}
 const data = {
     //labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     datasets: [{
         label: 'projName1', //PROJECT NAME
         data: [
-            {x: ['2022-05-02', '2022-05-15'], y: 'MODELLING', name: 'James', status: 'PLANNED', duration:'0', pay:'900'},
-            {x: ['2022-05-03', '2022-05-25'], y: 'ANIMATIC', name: 'Kirk', status: 'REVIEW1', duration:'0', pay:'100'},
-            {x: ['2022-05-02', '2022-05-05'], y: 'ANIMATION', name: 'Jasmine', status: 'DONE', duration:'0', pay:'250'},
-            {x: ['2022-05-12', '2022-05-30'], y: 'RENDER', name: 'John', status: 'WORKING', duration:'0', pay:'520'},
-            {x: ['2022-05-10', '2022-06-14'], y: 'COMP', name: 'Logan', status: 'PENDING', duration:'0', pay:'400'},
-        ], 
-        backgroundColor: 'rgba(10, 216, 82, 0.8)'
+            {x: ['2022-05-02', '2022-05-15'], y: 'MODELLING', name: 'James', status: 'PLANNED', duration:'0', pay:'900', note:''},
+            {x: ['2022-05-03', '2022-05-25'], y: 'ANIMATIC', name: 'Kirk', status: 'REVIEW', duration:'0', pay:'100', note:''},
+            {x: ['2022-05-02', '2022-05-05'], y: 'ANIMATION', name: 'Jasmine', status: 'DONE', duration:'0', pay:'250', note:''},
+            {x: ['2022-05-12', '2022-05-30'], y: 'RENDER', name: 'John', status: 'WORKING', duration:'0', pay:'520', note:''},
+            {x: ['2022-05-10', '2022-06-04'], y: 'COMP', name: 'Logan', status: 'PENDING', duration:'0', pay:'400', note:''},
+            {x: ['2022-05-10', '2022-06-10'], y: 'RENDER1', name: 'Logan', status: 'PENDING', duration:'0', pay:'400', note:''}
+        ],
+        //backgroundColor: (ctx) => { return color[ctx.raw.status];} 
+        //backgroundColor: 'rgba(10, 216, 82, 0.8)'
     }]
   };
 //----PLUGINS----------------------------
@@ -37,7 +46,7 @@ const data = {
             const { ctx, data, chartArea: { top, bottom, left, right }, scales: {x,y}} = chart;
             ctx.textBaseline = 'middle';
             data.datasets[0].data.forEach((datapoint, index) => {
-                ctx.fillText(datapoint.name, 10, y.getPixelForValue(index));
+                //ctx.fillText(datapoint.name, 10, y.getPixelForValue(index)); // THIS WORKS
             });
         }
     }
@@ -45,11 +54,13 @@ const data = {
         id: 'taskStatus',
         afterDatasetsDraw(chart, args, pluginOptions) {
             const { ctx, data, chartArea: { top, bottom, left, right }, scales: {x,y}} = chart;
+            ctx.save();
             data.datasets[0].data.forEach((datapoint, index) => {
-                ctx.fillText(datapoint.status, 15, y.getPixelForValue(index));
+                ctx.fillStyle = colorCode[datapoint.status];
+                ctx.fillText(datapoint.status, 80, y.getPixelForValue(index));
+                
             });
-            
-
+            ctx.restore();
         }
     }
 
@@ -58,13 +69,13 @@ const data = {
     type: 'bar',
     data,
     options: {
-      layout: {padding:{left:70}},
+      layout: {padding:{left:0}},
       indexAxis : 'y',
       scales: {
         x: {
             position: 'top',
             type: 'time',
-            time: {unit: 'day', display: true, text: 'date'},
+            time: {unit: 'day'},
             min: '2022-04-27', //PROJECT START DATE
             max: '2022-06-10' //PROJECT END DATE
         }
@@ -81,4 +92,4 @@ const data = {
     document.getElementById('myChart'),
     config
   );
-  
+  console.log(colorCode['PENDING'])
