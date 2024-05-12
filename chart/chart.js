e0857eec47137356b6e1f79dfb8e526d3b85ee39
hARRi <tranhoangtd@gmail.com>
2024-05-13 04:41:30 +0700

    1

--- chart/chart.js
+++ chart/chart.js
@@ -3,11 +3,9 @@ console.log("Chart.js " + Chart.version);
 //----TASK: BLOCKOUT, ANIMATIC, MODELLING, RIGGING, ANIMATION, VFX/LIGHT, RENDER, COMP, CLOSED
 
 const colorCode = {
-    PENDING:    'rgba(255, 26, 104, .7)',
-    PLANNED:    'rgba(54, 162, 235, .7)',
+    PENDING:    'rgba(255, 98, 179, .7)', 
     WORKING:    'rgba(255, 206, 86, .7)',
-    DONE:       'rgba(75, 192, 192, .7)',
-    UNKNOWN:    'rgba(153, 102, 255,.7)',
+    DONE:       'rgba(16, 255, 0, .7)',
     REVIEW:     'rgba(255, 159, 64, .7)'
 }
 const data = {
@@ -15,18 +13,17 @@ const data = {
     datasets: [{
         label: 'projName1', //PROJECT NAME
         data: [
-            {x: ['2022-05-02', '2022-05-15'], y: 'MODELLING', status: 'PLANNED', duration:'1', pay:'900', note:'furtherNote1'},
-            {x: ['2022-05-03', '2022-05-25'], y: 'ANIMATIC', status: 'REVIEW', duration:'2', pay:'100', note:''},
-            {x: ['2022-05-02', '2022-05-05'], y: 'ANIMATION', status: 'DONE', duration:'3', pay:'250', note:''},
-            {x: ['2022-05-12', '2022-05-30'], y: 'RENDER', status: 'WORKING', duration:'4', pay:'520', note:''},
-            {x: ['2022-05-10', '2022-06-04'], y: 'COMP', status: 'PENDING', duration:'4', pay:'400', note:''},
-            {x: ['2022-05-10', '2022-06-10'], y: 'RENDER1', status: 'PENDING', duration:'5', pay:'400', note:''}
+            {x: ['2022-05-02', '2022-05-15'], y: 'MODELLING', status: 'WORKING', duration:'1', note:'furtherNote1\nnewLine'},
+            {x: ['2022-05-03', '2022-05-25'], y: 'ANIMATIC', status: 'REVIEW', duration:'2', note:''},
+            {x: ['2022-05-02', '2022-05-05'], y: 'ANIMATION', status: 'DONE', duration:'3', note:''},
+            {x: ['2022-05-12', '2022-05-30'], y: 'RENDER', status: 'WORKING', duration:'4', note:''},
+            {x: ['2022-05-10', '2022-06-04'], y: 'COMP', status: 'PENDING', duration:'4', note:''},
+            {x: ['2022-05-10', '2022-06-10'], y: 'RENDER1', status: 'PENDING', duration:'5', note:''}
         ],
         backgroundColor: (ctx) => {return colorCode[ctx.raw.status];} 
     }]
-  };
-//----PLUGINS----------------------------
-    const todayLine = {
+};
+const todayLine = {
         id: 'todayLine',
         afterDatasetsDraw(chart, args, pluginOptions) {
             const { ctx, data, chartArea: { top, bottom, left, right }, scales: {x, y}} = chart;
@@ -38,19 +35,15 @@ const data = {
             ctx.lineTo(y.getPixelForValue(new Date()), bottom);
             ctx.stroke();
             ctx.restore();
-
             ctx.fillText(`${new Date().getDate()}-${new Date().getMonth()+1}`,y.getPixelForValue(new Date()), bottom);
-            }
-
-            
-    }
+            }  
+}
 
 //-----CONFIG------------------------------------------ 
-  const config = {
+const config = {
     type: 'bar',
     data,
     options: {
-      layout: {padding:{left:0}},
       indexAxis : 'y',
       scales: {
         x: {
@@ -61,24 +54,23 @@ const data = {
             max: '2022-06-10' //PROJECT END DATE
         }
       },
+      barPercentage: 0.6
+      ,
       plugins:{
         legend: {display:false},
         tooltip:{callbacks:{title:(ctx) => { //todo write duration function
             const start = new Date(ctx[0].raw.x[0])
             const end = new Date(ctx[0].raw.x[1])
             const duration = ctx[0].raw.duration
-            return `${start.toLocaleString([],{day:'numeric', month:'short'})} - ${end.toLocaleString([],{day:'numeric', month:'short'})}`
+            return `${start.toLocaleString([],{day:'numeric', month:'short'})} - ${end.toLocaleString([],{day:'numeric', month:'short'})} \n${ctx[0].raw.note}`
             }}},
         datalabels:{formatter: (val) => {return `${val.status}-${val.duration}days`}}
         }
     },
     plugins: [todayLine, ChartDataLabels],
-    //plugins:  [],
-    //options: {plugins:{datalabels:{}}}
-  };
+};
+const myChart = new Chart(document.getElementById('myChart'),config);
 
-  // render init block
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+//---FORM THAT LOAS TASKS INTO GANTT CHART -----------------------------
+
+//--FORM THAT UPADTE ITSELF------------------------------------------------
