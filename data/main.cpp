f3814b027b70f276b8ef8491fb76751a8730f8f5
hARRi <tranhoangtd@gmail.com>
2024-08-02 06:23:38 +0700

    1

--- data/main.cpp
+++ data/main.cpp
@@ -6,11 +6,23 @@ class Book {
         string title;
         bool avail1;
         int page;
+
+        Book (string aTitle, bool aAvail1, int aPage){
+            title = aTitle;
+            avail1 = aAvail1;
+            page = aPage;
+        }
+
 };
 
 int main()
-{
+{   
+    Book book1("SampleName1", true, 9);
+
+
     cout << "hell0!" << endl;
+
+    cout << book1.title;
     return 0;
 }
 
