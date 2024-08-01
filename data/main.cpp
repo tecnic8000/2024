47600e4244ca186ff87e17953b251a3c0464d4c0
hARRi <tranhoangtd@gmail.com>
2024-08-02 04:47:02 +0700

    1

--- data/main.cpp
+++ data/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
 using namespace std;
+
+class Book {
+    public:
+        string title;
+        bool avail1;
+        int page;
+};
 
 int main()
 {
     cout << "hell0!" << endl;
     return 0;
 }
+
