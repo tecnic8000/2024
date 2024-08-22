13f3afa8775c3cec8d416bdfefffcb17838cdc65
hARRi <tranhoangtd@gmail.com>
2024-08-22 18:13:23 +0700

    1

--- test/node01/http.js
+++ test/node01/http.js
@@ -1,8 +1,14 @@
 const http = require('http')
 const server = http.createServer((req,res)=>{
-    if(req.url === '/')
-        res.write('helloWorld fromNodeJS1---');
+    if(req.url === '/'){
+        res.write('helloWorld fromNodeJS1---'); 
         res.end();
+    }
+    else {
+        res.write('using other domain---1');
+        res.end();
+    }
+        
 });
 
 server.listen('3000');
