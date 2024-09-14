07be7609f74517637a586bdaeaa7f8adee329494
hARRi <tranhoangtd@gmail.com>
2024-09-14 11:39:26 +0700

    1

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
 function AboutTab() {
     return (
         <div>
-            <a href="https://discord.gg/cBRbkCvEm7" target="_blank"><i class="bi bi-discord"></i></a>
-            <a href="https://www.instagram.com/tecnic8000/" target="_blank"><i class="bi bi-instagram"></i></a>
-            <a href="https://youtube.com/@tecnic8000" target="_blank"><i class="bi bi-youtube"></i></a>
         </div>
         )
 }
 
 const about1 = ReactDOM.createRoot(document.getElementById('about1'));
-about1.render(<AboutTab />);
+//about1.render(<AboutTab />);
 
 
 
